03c2d58062b52fed03f7d6b6a3b57d0dcd6ed8e6
MrFatra <ramadhanfatra2004@gmail.com>
2025-10-23 21:48:14 +0700

    fix(textinput): forward ref prop

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -8,26 +8,30 @@ export type TextInputProps = {
     errorClassName?: string;
 } & InputHTMLAttributes<HTMLInputElement>
 
-export const TextInput: React.FC<TextInputProps> = ({
-    label,
-    id,
-    name,
-    error,
-    labelClassName,
-    errorClassName,
-    ...props
-}) => {
-    return (
-        <div className="w-full">
-            {label && <label htmlFor={id || name} className={cn("block text-sm font-medium mb-1 dark:text-white", labelClassName)}>{label}</label>}
-            <input
-                id={id || name}
-                name={name}
-                className={`block w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-primary focus:border-primary sm:text-sm dark:text-white dark:placeholder:text-white ${error ? 'border-error dark:border-error-dark' : 'border-secondary dark:border-secondary-dark'
-                    } ${props.disabled ? 'bg-secondary/30 cursor-not-allowed' : ''}`}
-                {...props}
-            />
-            {error && <p className={cn("mt-1 text-sm text-error", errorClassName)}>{error}</p>}
-        </div>
-    );
-};
+export const TextInput = React.forwardRef<HTMLInputElement, TextInputProps>(
+    ({
+        label,
+        id,
+        name,
+        error,
+        labelClassName,
+        errorClassName,
+        ...props
+    }, ref) => {
+        return (
+            <div className="w-full">
+                {label && <label htmlFor={id || name} className={cn("block text-sm font-medium mb-1 dark:text-white", labelClassName)}>{label}</label>}
+                <input
+                    ref={ref}
+                    id={id || name}
+                    name={name}
+                    className={`block w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-primary focus:border-primary sm:text-sm dark:text-white dark:placeholder:text-white ${error ? 'border-error dark:border-error-dark' : 'border-secondary dark:border-secondary-dark'
+                        } ${props.disabled ? 'bg-secondary/30 cursor-not-allowed' : ''}`}
+                    {...props}
+                />
+                {error && <p className={cn("mt-1 text-sm text-error", errorClassName)}>{error}</p>}
+            </div>
+        )
+    })
+
+TextInput.displayName = 'TextInput';
